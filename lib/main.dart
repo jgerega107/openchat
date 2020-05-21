d43c17f1c81728d87c58c75215585f9a758f8d48
Jacob Gerega <22921310+jgerega107@users.noreply.github.com>
2020-05-21 01:16:07 -0400

    Added preliminary Google sign in option

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 void main() => runApp(MyApp());
 
@@ -18,14 +21,46 @@ class MyApp extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.all(20),
-                child: Text("openchat", textAlign: TextAlign.center, style: TextStyle(fontFamily: "inconsolata"), textScaleFactor: 2,),
+                child: Text("welcome to openchat.", textAlign: TextAlign.center, style: TextStyle(fontFamily: "inconsolata"), textScaleFactor: 2,),
               ),
-              Image(image: AssetImage("assets/images/placeholder.png"),)
+              Image(image: AssetImage("assets/images/placeholder.png"),
+              ),
+              _GoogleSignInSection()
             ],
             mainAxisAlignment: MainAxisAlignment.center,
           )
         ),
       ),
     );
+  }
+}
+
+class _GoogleSignInSection extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _GoogleSignInSectionState();
+}
+
+class _GoogleSignInSectionState extends State<_GoogleSignInSection>{
+    final GoogleSignIn _googleSignIn = GoogleSignIn();
+    final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(padding: EdgeInsets.all(30),
+     child: Column(
+      children: <Widget>[
+        SignInButton(Buttons.Google, onPressed: () {
+          _handleSignIn();
+        })
+      ],
+    ));
+  }
+
+  void _handleSignIn() async {
+    final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+    final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+    print("signed in " + user.displayName);
   }
 }
